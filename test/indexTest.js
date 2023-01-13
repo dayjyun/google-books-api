6f1174069eac08bfca01b226a3ae4d6727fc087e
Kevin Barrios <kevinbarrios@ymail.com>
2023-01-13 03:35:06 -0600

    final updates with addToReadingList

--- test/indexTest.js
+++ test/indexTest.js
@@ -6,12 +6,13 @@ import addToReadingList from "../index.js";
 
 describe('addToReadingList', () => {
     // length of reading list should increase by 1 when book is added
-    // it('Should add a book to the reading list', () => {
-    //     let readingList = []
-    //     let searchQuery = [{id: 1, volumeInfo: {title: "Book One", authors: "Author Wright", publisher: "Prestigious Publishing"}}]
-    //     assert.equal(addToReadingList(searchQuery))
-    //     assert.equal(readingList.length, 1)
-    // });
+    it('Should add a book to the reading list', () => {
+        let readingList = []
+    let searchQuery = [{id: 1, volumeInfo: {title: "Book One", authors: "Author Wright", publisher: "Prestigious Publishing"}}]
+        addToReadingList(searchQuery, readingList);
+        assert.strictEqual(readingList.length, 1)
+    assert.deepEqual(readingList[0], {id: 1, volumeInfo: {title: "Book One", authors: "Author Wright", publisher: "Prestigious Publishing"}})
+    });
 
     // book should not be added if the input is not a number between 1 and 5
     it('Should not add a book if the input is not a number between 1 and 5', () => {
