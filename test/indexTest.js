37e1661f5f91e22374506566eeb69715cbaba34c
Kevin Barrios <kevinbarrios@ymail.com>
2023-01-13 03:20:31 -0600

    created test for book not to be added if number is not in required parameters

--- test/indexTest.js
+++ test/indexTest.js
@@ -5,14 +5,20 @@ import assert from "assert";
 import addToReadingList from "../index.js";
 
 describe('addToReadingList', () => {
+    // length of reading list should increase by 1 when book is added
     it('Should add a book to the reading list', () => {
-        // length of reading list should increase by 1
-        // book should be added to the reading list
-        // book should not be added if it is already in the reading list
-        // book should not be added if the input is not a number between 1 and 5
         let readingList = []
-        let searchQuery = [{id: 1, volumeInfo: {title: "The Hobbit", authors: "J.R.R. Tolkien", publisher: "Allen & Unwin"}}]
+        let searchQuery = [{id: 1, volumeInfo: {title: "Book One", authors: "Author Wright", publisher: "Prestigious Publishing"}}]
         assert.equal(addToReadingList(searchQuery))
         assert.equal(readingList.length, 1)
+    });
+    
+    // book should not be added if the input is not a number between 1 and 5
+    it('Should not add a book if the input is not a number between 1 and 5', () => {
+        let readingList = []
+        let searchQuery = [{id: 6, volumeInfo: {title: "Book One", authors: "Author Wright", publisher: "Prestigious Publishing"}}]
+        addToReadingList(searchQuery, readingList);
+        assert.equal(readingList.length, 0)
+
     })
 })
